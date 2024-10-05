953420b7fd6d80088386b21d22208385212a7f04
MrSumTing <47291853+MrSumTing@users.noreply.github.com>
2024-10-05 23:33:23 +0200

    register

--- customers/serializers.py
+++ customers/serializers.py
@@ -42,26 +42,30 @@ class LoginSerializer(serializers.Serializer):
 
 
 class CustomerRegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(write_only=True)
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    first_name = serializers.CharField(write_only=True)
+    last_name = serializers.CharField(write_only=True)
+    address = serializers.CharField(write_only=True, source='address_line')
 
     class Meta:
         model = Customer
-        fields = ['email', 'username', 'password', 'first_name', 'last_name', 'address', 'postal_code', 'city']
+        fields = [
+            'email', 'username', 'password', 'first_name', 'last_name',
+            'address', 'postal_code', 'city'
+        ]
 
     def create(self, validated_data):
-
-        user = User.objects.create_user(
-            email=validated_data['email'],
-            password=validated_data['password'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            username=validated_data['username']
-        )
-        customer = Customer.objects.create(
-            user=user,
-            address_line=validated_data['address'],
-            postal_code=validated_data['postal_code'],
-            city=validated_data['city']
-        )
+        user_data = {
+            'email': validated_data.pop('email'),
+            'username': validated_data.pop('username'),
+            'password': validated_data.pop('password'),
+            'first_name': validated_data.pop('first_name'),
+            'last_name': validated_data.pop('last_name'),
+        }
+        user = User.objects.create_user(**user_data)
+        customer = Customer.objects.create(user=user, **validated_data)
         return customer
 
 
