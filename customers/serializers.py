65065e65d247c626f2ea2f5472d0f2533ecc5145
MrSumTing <47291853+MrSumTing@users.noreply.github.com>
2024-10-05 15:53:20 +0200

    RegisterSerializer

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from .models import Customer, DiscountCode
 
 class DiscountCodeSerializer(serializers.ModelSerializer):
@@ -25,5 +24,14 @@ class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True)
 
+
+class CustomerRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['email', 'password', 'first_name', 'last_name', 'phone_number', 'address', 'postal_code', 'city']
+
+    def create(self, validated_data):
+        customer = Customer.objects.create_user(**validated_data)
+        return customer
 
 
