fe5d306beb002b12b45d19be8885263af5141c2d
MrSumTing <47291853+MrSumTing@users.noreply.github.com>
2024-10-05 16:34:44 +0200

    Simpler urls

--- PapaLuigisDeliciousPizza/urls.py
+++ PapaLuigisDeliciousPizza/urls.py
@@ -23,15 +23,15 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     #STAYING AUTHENTICATED!
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('admin/', admin.site.urls),
-    path('api/customers/', include('customers.urls')),
-    path('api/orders/', include('orders.urls')),
-    path('api/menu/', include('menu.urls')),
+    path('customers/', include('customers.urls')),
+    path('orders/', include('orders.urls')),
+    path('menu/', include('menu.urls')),
    # path('api/delivery/', include('delivery.urls')),
 
 # API visualisation!
-   path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-   path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+   path('schema/', SpectacularAPIView.as_view(), name='schema'),
+   path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
