ddb727bafa14eba3480d08e648aeb4e09976b166
MrSumTing <47291853+MrSumTing@users.noreply.github.com>
2024-10-05 16:28:14 +0200

    Register added

--- customers/urls.py
+++ customers/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import LoginView
-
-router = DefaultRouter()
-router.register(r'login', LoginView, basename="LoginViews")
+from .views import LoginView, CustomerRegisterView
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
+    path('register/', CustomerRegisterView.as_view()),
 ]
--- customers/views.py
+++ customers/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from customers.serializers import CustomerRegisterSerializer
+
 
 class LoginView(APIView):
     def post(self, request):
@@ -23,3 +25,11 @@ class LoginView(APIView):
                 {'detail': 'Invalid email or password.'},
                 status=status.HTTP_401_UNAUTHORIZED
             )
+
+class CustomerRegisterView(APIView):
+    def post(self, request):
+        serializer = CustomerRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
