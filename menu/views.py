be98702611628e68e07b18c5b441aa52257f7336
Mihai Solescu <mihai.solescu@outlook.com>
2024-10-07 00:28:46 +0200

    get all pizzas prototype working

--- menu/views.py
+++ menu/views.py
@@ -1,14 +1,25 @@
-from requests import Response
 from rest_framework import viewsets
 from rest_framework.views import APIView
 
-from .models import Pizza, Ingredient
-from .serializers import PizzaSerializer, IngredientSerializer
+from .models import Pizza, Ingredient, Dessert, Drink
+from .serializers import PizzaSerializer, IngredientSerializer, DessertSerializer, DrinkSerializer
 
-class PizzaView(viewsets.ReadOnlyModelViewSet):
-    queryset = Pizza.objects.select_related('')
+class PizzaListViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    A viewset for viewing a list of pizzas and their details.
+    """
+    queryset = Pizza.objects.all()
     serializer_class = PizzaSerializer
 
-class IngredientView(viewsets.ReadOnlyModelViewSet):
+
+class IngredientListView(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+
+class DesertListView(APIView):
+    queryset = Dessert.objects.all()
+    serializer_class = DessertSerializer
+
+class DrinkListView(APIView):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
